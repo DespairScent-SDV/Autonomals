b1e930fb3cd9de5586c405ba52e207bbf4d0325d
DespairScent <74109497+DespairScent@users.noreply.github.com>
2024-05-02 03:30:58 +0300

    Initial commit

---
+++ ModEntry.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using Microsoft.Xna.Framework;
+using StardewValley.Buildings;
+using StardewValley.Locations;
+using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.CompilerServices;
+
+namespace DespairScent.Autonomals
+{
+    internal sealed class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+
+            harmony.Patch(AccessTools.Method(typeof(FarmAnimal), nameof(FarmAnimal.updateWhenNotCurrentLocation)),
+               prefix: new HarmonyMethod(typeof(ModEntry), nameof(PatchFarmAnimalsUpdate)));
+        }
+
+        private static bool PatchFarmAnimalsUpdate(FarmAnimal __instance, Building currentBuilding, GameTime time, GameLocation environment)
+        {
+            if (environment == Game1.getFarm() && currentBuilding == null)
+            {
+                __instance.updateWhenCurrentLocation(time, environment);
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
